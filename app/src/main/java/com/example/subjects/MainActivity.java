e21fe4a197617ff46f01058107979be7bbbaee1d
vivek <tellvivk@gmail.com>
2018-03-07 19:52:55 +0530

    added firebase auth

--- app/src/main/java/com/example/subjects/MainActivity.java
+++ app/src/main/java/com/example/subjects/MainActivity.java
@@ -1,27 +1,75 @@
 package com.example.subjects;
 
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.example.subjects.app.drawing.DrawingBoardActivity;
 import com.example.subjects.app.home.HomeActivity;
+import com.example.subjects.databinding.ActivityMainBinding;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final int RC_SIGN_IN = 123;
+
+    private ActivityMainBinding dataBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        new Handler().postDelayed(new Runnable() {
+        // Choose authentication providers
+        final List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build());
+
+
+        dataBinding.btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-
-                HomeActivity.start(MainActivity.this);
-
-                finish();
+            public void onClick(View v) {
+                // Create and launch sign-in intent
+                startActivityForResult(
+                        AuthUI.getInstance()
+                                .createSignInIntentBuilder()
+                                .setAvailableProviders(providers)
+                                .build(),
+                        RC_SIGN_IN);
             }
-        }, 1000);
+        });
+
+
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                Log.d(TAG, "onActivityResult: "+user);
+                // ...
+            } else {
+                Log.d(TAG, "onActivityResult: sign in failed");
+            }
+        }
+    }
+
+
 }
